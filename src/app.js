87f4c0232614ff737dccf1808de41141c286e372
Liza Kuchmenko <elizabeth.kuchmenko@gmail.com>
2022-04-28 17:35:43 +0200

    functionality for date update

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
 let apiKey = 'b28509bf2e3b7243f21402b7bfc8dac4';
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+let city = 'Rome';
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
+let dateEl = document.querySelector('#date');
 let descriptionEl = document.querySelector('#description');
 let tempEl = document.querySelector('#temperature');
 let cityEl = document.querySelector('#city');
 let humidityEl = document.querySelector('#humidity');
 let windEl = document.querySelector('#wind');
+
+function formatDate(timestamp) {
+	let week = [
+		'Sunday',
+		'Monday',
+		'Tuesday',
+		'Wednesday',
+		'Thursday',
+		'Friday',
+		'Saturday',
+	];
+	let date = new Date(timestamp);
+	let hours = date.getHours();
+	if (hours < 10) {
+		hours = `0${hours}`;
+	}
+	let minutes = date.getMinutes();
+	if (minutes < 10) {
+		minutes = `0${minutes}`;
+	}
+	let day = week[date.getDay()];
+	return `${day} ${hours}:${minutes}`;
+}
 
 function displayTemperature(response) {
 	descriptionEl.innerHTML = response.data.weather[0].description;
@@ -13,6 +38,7 @@ function displayTemperature(response) {
 	cityEl.innerHTML = response.data.name;
 	humidityEl.innerHTML = response.data.main.humidity;
 	windEl.innerHTML = Math.round(response.data.wind.speed);
+	dateEl.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 axios.get(apiUrl).then(displayTemperature);
